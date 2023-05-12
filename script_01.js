2b9ea3c188ac2609c24b406534e1d7e8339aaecc
Ozan Oezyurt <ozan.oezyurt@icloud.com>
2023-05-12 10:15:12 +0200

    4 Rechenarten

--- script_01.js
+++ script_01.js
@@ -8,17 +8,60 @@
 1. Dateneingabe + -überprüfung : 
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+4. Ausgabe in Konsole : erledigt!
 */
 
+// module: addition a + b |  test:
+
+output(add(2,2));
+output(add(2,-2));
+output(add(2,0));
+
+function add(a , b) {
+let c = a + b;	
+return a + b;	
+}
+
+// module: substract a - b |  test:
+
+output(substract(2,2));
+output(substract(4,-2));
+output(substract(2,0));
+
+function substract(a , b) {
+let c = a - b;	
+return a - b;
+}
+
+// module: multiply a * b |  test:
+
+output(multiply(2,2));
+output(multiply(2,-2));
+output(multiply(2,0));
+
+function multiply(a , b) {
+let c = a * b;
+return a * b;
+}
+
+// module: divide a / b |  test:
+
+output(divide(2,2));
+output(divide(2,-2));
+output(divide(2,0));
+
+function divide(a , b) {
+let c = a / b;
+return a / b;	
+}
 
 
 
 
-// module: output | test:
-// output("hello");
-// output(2);
+
+
+
+// 4. module: output | test:
 function output(outputData) {
 	console.log(outputData);
 }
-  
